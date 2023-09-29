043c390f1f97ca8771f5127e6750fee57189eb89
Elizabeth <elizabeth.muthoni@student.moringaschool.com>
2023-09-29 04:41:13 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const messages = [];
+    
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+    
+    return messages;
+  }
+
+  function countDown(startingNumber) {
+    
+    if (startingNumber <= 0 || !Number.isInteger(startingNumber)) {
+      console.log("Please provide a positive integer as input.");
+      return;
+    }
+  
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
